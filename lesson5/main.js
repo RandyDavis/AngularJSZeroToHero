10119ac88b4e485c01329b0265feb81b810a4acd
Randy Davis <randy_davis@outlook.com>
2016-01-12 01:08:26 -0600

    complete form validation using core angularJS

---
+++ lesson5/main.js
@@ -0,0 +1,16 @@
+angular.module('minmax', [])
+		.controller('MinMaxCtrl', ["$scope", "$http", function ($scope, $http) {
+			$scope.formModel = {};
+
+			$scope.onSubmit = function () {
+				console.log("Hey i'm submitted!");
+				console.log($scope.formModel);
+
+				//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+				//success(function (data) {
+				//	console.log(":)")
+				//}).error(function(data) {
+				//	console.log(":(")
+				//});
+			};
+		}]);
---
+++ lesson6/main.js
@@ -0,0 +1,21 @@
+angular.module('minmax', [])
+		.controller('MinMaxCtrl', ["$scope", "$http", function ($scope, $http) {
+			$scope.formModel = {};
+
+			$scope.onSubmit = function (valid) {
+				if (valid) {
+
+				console.log("Hey i'm submitted!");
+				console.log($scope.formModel);
+
+				//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+				//success(function (data) {
+				//	console.log(":)")
+				//}).error(function(data) {
+				//	console.log(":(")
+				//});
+				} else {
+					console.log("Invalid Form!");
+				}
+			};
+		}]);
