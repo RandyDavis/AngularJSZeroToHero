22c84be2e55f4675ae100d52c7d09f5a3b673fcd
Randy Davis <randy_davis@outlook.com>
2016-01-13 23:45:06 -0600

    finish understanding scope: Root Scope section

---
+++ section4/lesson2/main.js
@@ -0,0 +1,16 @@
+var app = angular.module('codecraft', []);
+
+app.controller('ParentController', function ($scope, $rootScope) {
+	//$scope.name = "Parent";
+	//$scope.reset = function () {
+	//	$scope.name = "Parent";
+	//}
+});
+
+app.controller('ChildController', function ($scope, $rootScope) {
+	$scope.reset = function () {
+		$rootScope.name = "Reset By Child";
+	}
+});
+
+
